05e88945e007ea81a460ea79ec17300db1b95896
Dinesh vaish <dineshbais2005@gmail.com>
2026-07-29 19:42:52 +0530

    Time: 1929 ms (6.54%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,17 @@
+SELECT
+
+    v.customer_id,
+
+    COUNT(*) AS count_no_trans
+
+FROM Visits v
+
+WHERE v.visit_id NOT IN (
+
+    SELECT visit_id
+
+    FROM Transactions
+
+)
+
+GROUP BY v.customer_id;
