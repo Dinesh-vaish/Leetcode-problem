3bfdc551caf15467d0904690ede467e5ef83f09c
Dinesh vaish <dineshbais2005@gmail.com>
2026-08-08 13:37:02 +0530

    Time: 46 ms (54.26%), Space: 104 MB (62.77%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -5,15 +5,10 @@ public:
         int m = word2.size();
 
         vector<int> ans;
-        
-        // last[j] = word1 me word2[j] ko match karne
-        // ke liye latest possible index
         vector<int> last(m, -1);
 
         int i = n - 1;
         int j = m - 1;
-
-        // Right se matching positions find karo
         while (i >= 0 && j >= 0) {
             if (word1[i] == word2[j]) {
                 last[j] = i;
